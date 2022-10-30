e6e37bee72460f617284b5d57f74a6ec7cfe511a
dluca22 <luc.due@tim.it>
2022-10-30 14:42:39 +0100

    added base template for login/register route

--- keymdall/TODO.md
+++ keymdall/TODO.md
@@ -1,4 +1,19 @@
 
+## TO Do:
+
+~~1. build registration and login page into one with toggle between views.~~
+2. Add logic to registration, adding instances of user in db after successful registration
+
+<br>
+
+## log
+* [30/10]
+    * Created the base of the project, started new django project from zero, linked the views, the settings, the urls from the main project to the vault app
+    * added temporary icon
+    * *added user snippets in vscode for django templates*
+    * added templates with navbar links with conditional for logged in users
+    * created the register route with a button that switches the login form to registration asyncronous to avoid refresh and additional templates.
+    * added layout inheritance and stylesheet and script files in static
 
 
 
--- keymdall/vault/static/vault/script.js
+++ keymdall/vault/static/vault/script.js
@@ -1,14 +1,29 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-    const url = new.URL(document.URL);
+    const url = new URL(document.URL);
     const base_path = url.pathname.split("/")[1];
 
     if (base_path== "login"){
-        login_page();
+        login_toggle();
     }
 }); /*end of DOMContentLoaded */
 
 
-function login_page(){
-    const button = document.querySelector
+function login_toggle(){
+    const toggle = document.querySelector('#toggle-view');
+    toggle.onclick = () =>{
+        if (toggle.value == "register_form"){
+            document.querySelector("#login_form").style.display = "none";
+            document.querySelector("#register_form").style.display = "block";
+            toggle.value = "login_form"
+            toggle.textContent = "Log-in"
+        }
+        else {
+            document.querySelector("#login_form").style.display = "block";
+            document.querySelector("#register_form").style.display = "none";
+            toggle.value = "register_form"
+            toggle.textContent = "Register"
+            
+        }
+    }
 }
